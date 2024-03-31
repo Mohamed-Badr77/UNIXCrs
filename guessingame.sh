ae76c7e396f85cf6c841563ec1449cdbe1f722c3
Mohamed-Badr77 <126291482+Mohamed-Badr77@users.noreply.github.com>
2024-03-31 13:56:53 +0000

    Add files via upload

---
+++ guessingame.sh
@@ -0,0 +1,27 @@
+function get_files_in_current_directory {
+	files_count=$(ls | wc -l)
+}
+
+
+get_files_in_current_directory
+
+echo "How many files are in current directory?"
+echo "Enter your guess: "
+read  guess
+
+while [ $guess -ne $files_count ]
+do
+	if [ $guess -eq $files_count ]
+	then
+		break
+	elif [ $guess -lt $files_count ]
+	then
+		echo "$guess is too low"
+	else
+		echo "$guess is too high"
+	fi
+
+	echo "Guess again: "
+	read guess
+done
+echo "Congratulations! $guess files are in current directory"
